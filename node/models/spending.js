a7532b4de245ba0d6e79135f9f6279ce6f636d8f
mphipps2017 <mnphipps98@gmail.com>
2019-05-17 21:22:50 -0500

    Cleaned up some code

--- node/models/spending.js
+++ node/models/spending.js
@@ -4,13 +4,19 @@ const mongo = require('../helpers/mongoUtil.js');
 
 const collectionName = 'spending';
 
-//Inserts new User into the users collection
+//Inserts new Spending Year
 exports.new = (data, callback) => {
     mongo.getDb().collection(collectionName).insertOne({
-        userID: ObjectId(data.id),
+        userID: data.id,
         year: data.year,
         months: {}, //edit this to include the object as definned in my project plans
     }, function(err, res) {
         if (err) throw err;
     });
 };
+
+exports.get = (id, callback) => {
+    mongo.getDb().collection(collectionName).findOne({ _id: ObjectId(id) }, (err, result) => {
+      callback(err, result);
+    });
+  };
--- node/routes/spending.js
+++ node/routes/spending.js
@@ -2,20 +2,22 @@ const express = require('express');
 const spendingModel = require('../models/spending.js');
 
 const router = express.Router();
-
-//Grab a spending document (based on document ID)
-router.get('/spending', (req, res, next) => {
-  res.json({ success: true });
-  console.log('hit');
-});
   
-//Create a new User
-router.post('/spending', (req, res) => {
-  spendingModel.new(req.query, (err) => { // req.query gives the body of the post request
+//Create a new Spending Year for a given user
+router.post('/', (req, res) => {
+  spendingModel.new(req.query, (err) => {
     if (err) return next(err);
     res.json({ success: true });
   });
   res.send(req.query);
+});
+
+//Get a spending year by it's document ID
+router.get('/:id', (req, res, next) => {
+  spendingModel.get(req.params.id, (err, value) => {
+    if (err) return next(err);
+    res.json(value);
+  });
 });
 
 module.exports = router;
